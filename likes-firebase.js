c5c2330d92dea684c3ec784e8c73a7bf13db622b
Tonio103 <antoine.nils.albertini@gmail.com>
2026-04-12 17:26:12 +0200

    Update likes-firebase.js

--- likes-firebase.js
+++ likes-firebase.js
@@ -1,4 +1,4 @@
-aimport { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
 import {
   getFirestore,
   doc,
