260515dd494b9a48af07fc7edb7265ec46f17f5c
Tonio103 <antoine.nils.albertini@gmail.com>
2026-04-18 00:50:09 +0200

    Update site.js
    ééééé

--- site.js
+++ site.js
@@ -276,7 +276,7 @@ jusorange: {
       { label: "Fondants", keywords: ["fondant", "fondants", "chocolat", "coulant"], url: "fondant.html" },
       { label: "Marbré", keywords: ["marbre", "marbré", "gateau marbre", "gâteau marbré", "cake"], url: "gateau-marbre.html" },
       { label: "Poké bowl", keywords: ["poke", "pokebowl", "poké", "poké bowl", "poke bowl", "saumon"], url: "poke-bowl.html" },
-      { label: "Jus d’orange", keywords: ["jus", "orange", "jus d'orange", "boisson", "bio"], url: "jus-orange.html" }
+      { label: "Jus d’orange", keywords: ["jus", "orange", "jus d'orange", "boisson", "bio"], url: "jusorange.html" }
     ];
 
     function hideSuggestions() {
